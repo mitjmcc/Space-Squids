39e45696f5f0abe4211a8b1b666cc28b4588cbfc
Dhruv Karunakaran <dhruvn@gmail.com>
2015-09-13 13:12:12 -0400

    unity 5.2 updated

--- Assets/Scripts/tentacleSegment.cs
+++ Assets/Scripts/tentacleSegment.cs
@@ -9,10 +9,12 @@ public class tentacleSegment : MonoBehaviour {
 	public float timer;
 	float intensity;
 	public int player;
+    float lerpT;
+    float lerpSpeed = 0.1f;
 	// Use this for initialization
 	void Start () {
 		timer = 0;
-		intensity = 0.75f;
+		intensity = 1f;
 	}
 	
 	// Update is called once per frame
@@ -26,9 +28,26 @@ public class tentacleSegment : MonoBehaviour {
 		if(timer >= (Mathf.PI*2)) timer = 0;
 
 		float rotateAngle = -intensity * Mathf.Cos (timer + segment / intensity);
+        //Debug.Log(Input.GetAxis("Vertical" + player));
+        if (Input.GetAxis("Pitch" + player) != 0 || Input.GetAxis("Vertical" + player) != 0)
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Vertical" + player) * rotateAngle, Input.GetAxis("Pitch" + player) * rotateAngle));
+        }
+        else
+        {
+            /*
+            lerpT = 0;
+            while (lerpT < 1)
+            {
+                transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, new Vector3(0, -90, 0), lerpT);
+                lerpT += Time.deltaTime * lerpSpeed;
+            }
+            */
+            Vector3 startState = Vector3.zero + transform.root.eulerAngles; 
+            transform.eulerAngles = startState;
+            
 
-		transform.Rotate (new Vector3 (0,Input.GetAxis("Pitch" + player) * rotateAngle,Input.GetAxis("Vertical" + player) * rotateAngle));
-
+        }
 
 	}
 }
